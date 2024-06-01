37f97cc0c71ecac33eb022c6271d11912e412192
reza <hosseini.reza65@gmail.com>
2024-06-01 09:40:11 +0330

    book app

--- src/bookhub/apps/books/views.py
+++ src/bookhub/apps/books/views.py
@@ -1,12 +1,17 @@
+from django.db.models import Prefetch
+
 from rest_framework import viewsets
 from rest_framework.exceptions import NotAcceptable
 
 from django_filters import rest_framework as filters
 
+from bookhub.apps.authors.models import Author
 from bookhub.apps.books.filters import BookFilter
 from bookhub.apps.books.models import Category, Book
 from bookhub.apps.books.serializers import CategoryListSerializer, CategoryDetailSerializer, BookListSerializer, \
     BookDetailSerializer
+from bookhub.apps.publishers.models import Publisher
+from bookhub.apps.translators.models import Translator
 
 
 # Create your views here.
@@ -45,3 +50,22 @@ class BookViewSet(viewsets.ReadOnlyModelViewSet):
 
             case _:
                 raise NotAcceptable()
+
+    def get_queryset(self):
+        """
+        Retrieve the queryset of books with related authors, publishers, translators, and category.
+
+        Returns:
+                QuerySet: A queryset of Book objects with select_related and prefetch_related applied on related models.
+        """
+        author_prefetch = Prefetch('author',
+                                   queryset=Author.objects.all())
+        publisher_prefetch = Prefetch('publisher',
+                                      queryset=Publisher.objects.all())
+        translator_prefetch = Prefetch('translator',
+                                       queryset=Translator.objects.all())
+        return Book.objects.is_public().select_related('category').prefetch_related(
+            author_prefetch,
+            publisher_prefetch,
+            translator_prefetch
+        )
